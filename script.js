51c6fd98dcb8dd32cb9ccbcd1276ced680a9f19d
Himanshu Mishra <himanshu.live7@gmail.com>
2024-05-03 16:31:29 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+const overdose = new Audio('./songs/song1.mp3');
+const metamorphosis = new Audio('./songs/metamorphosis.mp3');
+const squeakyAudio = new Audio('./songs/song2.mp3');
+const hopeAudio = new Audio('./songs/song3.mp3');
+const janjiAudio = new Audio('./songs/overdose.mp3');
+
+const prevBtn = document.querySelector('.previous');
+const playBtn = document.querySelector('.play-pause');
+const nextBtn = document.querySelector('.next');
+const songName = document.querySelector('.song-name');
+const playPauseIcon = document.querySelector('#play-pause-icon');
+
+
+const songs = [
+  { ele: overdose, audioName: 'Overdose' },
+  { ele: metamorphosis, audioName: 'Metamorphosis' },
+  { ele: squeakyAudio, audioName: 'Squeaky Sound' },
+  { ele: hopeAudio, audioName: 'Hope by NCS' },
+  { ele: janjiAudio, audioName: 'Janji by NCS' },
+];
+
+for(const song of songs) {
+  song.ele.addEventListener('ended', ()=> {
+    updateSong('next');
+    playPauseSong();
+  })
+}
+
+let current = 0;
+let currentSong = songs[current].ele;
+songName.textContent = songs[current].audioName;
+
+playBtn.addEventListener('click',()=> {
+  playPauseSong();
+})
+
+nextBtn.addEventListener('click', () => {
+  updateSong('next');
+  playPauseSong();
+});
+
+prevBtn.addEventListener('click', () => {
+  updateSong('prev');
+  playPauseSong();
+});
+
+const updateSong = (action)=> {
+  currentSong.pause();
+  currentSong.currentTime = 0;
+
+  if(action === 'next'){
+    current++;
+    if(current > songs.length -1) current = 0;
+  }
+  if(action === 'prev'){
+    current--;
+    if(current < 0) current = songs.length - 1;
+  }
+  currentSong = songs[current].ele;
+  songName.textContent = songs[current].audioName;
+}
+
+const playPauseSong = ()=> {
+  if(currentSong.paused){
+    currentSong.play();
+    playPauseIcon.className = 'ph-bold ph-pause';
+  }
+  else {
+    currentSong.pause();
+    playPauseIcon.className = 'ph-bold ph-play';
+  }
+}
